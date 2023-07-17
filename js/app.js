ca0bb89fd22a4d4c647b66f17fcaf3deef24c848
matynjr <akua@AKUAs-MacBook-Pro.local>
2023-07-17 10:54:48 +0300

    added ability for data  to persit on page reload

--- js/app.js
+++ js/app.js
@@ -1,30 +1,47 @@
-// get the Element required ie input and listing tasks
+// get the Element required, i.e., input and listing tasks
 const inputTask = document.getElementById("inputTask");
 const taskList = document.getElementById("taskList");
 
-//create a function that adds a task to a list
-
+// create a function that adds a task to a list
 function addTask() {
-  //get what is typed into the input field
+  // get what is typed into the input field
   const inputTaskText = inputTask.value;
 
-  //check to see if there is an item stored in browsers local storage.
+  // check to see if there is an item stored in the browser's local storage
   if (localStorage.getItem("myStoredListItemsString")) {
-    //if item exists its parsed into a json object and stored in items variable
+    // if the item exists, it is parsed into a JSON object and stored in the 'items' variable
     let items = JSON.parse(localStorage.getItem("myStoredListItemsString"));
-    //add an new item that got from the input to the items list
+    // add a new item that was obtained from the input to the 'items' list
     items.push(inputTaskText);
-    //store the updated items into a string and save them back to local storage
+    // store the updated 'items' into a string and save them back to local storage
     localStorage.setItem("myStoredListItemsString", JSON.stringify(items));
   } else {
-    //else if there is nothing in the local storage list set it
+    // else, if there is nothing in the local storage list, set it
     localStorage.setItem(
       "myStoredListItemsString",
       JSON.stringify([inputTaskText])
     );
   }
 
-  console.log(localStorage.getItem("myStoredListItemsString"));
+  // displaying the stored list
+  const retrievedItems = localStorage.getItem("myStoredListItemsString");
 
+  // convert the retrieved items string back to an array
+  const itemsArray = JSON.parse(retrievedItems);
+
+  // clear the task list before adding the updated tasks
+  taskList.innerHTML = "";
+
+  // loop through the items array and create list items for each task
+  itemsArray.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    taskList.appendChild(listItem);
+  });
+
+  // clear input field after adding a task
   inputTask.value = "";
 }
+
+// call the addTask function when the page finishes loading
+window.addEventListener("load", addTask);
